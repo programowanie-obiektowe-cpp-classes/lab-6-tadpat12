c73cc66c2f3fda955d58e9f13b0c7ccca7d10dc4
tadpat12 <tadpat12@gmail.com>
2024-12-02 17:29:45 +0100

    1

--- foo.hpp
+++ foo.hpp
@@ -9,8 +9,12 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-
+   
     // Twoja implementacja tutaj
-
+    std::for_each(people.begin(),people.end(),[](Human& obj){obj.birthday();});
+    std::transform(people.begin(), people.end(), retval.rbegin(), [](const Human& h) {
+        return h.isMonster() ? 'n' : 'y';
+    });
+    retval.resize(people.size());
     return retval;
 }
